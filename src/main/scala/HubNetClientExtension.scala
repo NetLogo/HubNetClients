b64b8732eff9871700441e53020bb3a5ba7c56dd
joshcough <joshcough@gmail.com>
2011-06-25 09:26:20 -0500

    added click-button prim and cleaned up the code

--- src/main/scala/HubNetClientExtension.scala
+++ src/main/scala/HubNetClientExtension.scala
@@ -1,9 +1,17 @@
 import org.nlogo.api._
 import org.nlogo.api.Syntax._
-import org.nlogo.api.ScalaConversions._
 
 object HubNetClientExtension {
   var client: Option[BasicClient] = None
+  def connect(user:String, ip:String, port:Int){
+    if(client.isDefined) sys.error("already connected. use disconnect first.")
+    client = Some(BasicClient(user, "COMPUTER", ip, port))
+  }
+  def disconnect(){ client.foreach(_.close("feel like it.")) }
+  def send(widgetName:String, newValue:AnyRef) {
+    if(!client.isDefined) sys.error("not connected")
+    client.foreach( _.sendActivityCommand("Button", widgetName, newValue))
+  }
 }
 
 class HubNetClientExtension extends DefaultClassManager {
@@ -11,43 +19,32 @@ class HubNetClientExtension extends DefaultClassManager {
     manager.addPrimitive("connect", new Connect)
     manager.addPrimitive("disconnect", new Disconnect)
     manager.addPrimitive("send", new Send)
+    manager.addPrimitive("click-button", new ClickButton)
   }
 }
 
 class Connect extends DefaultCommand {
   override def getSyntax = commandSyntax(Array(TYPE_STRING, TYPE_STRING, TYPE_NUMBER))
   def perform(args: Array[Argument], context: Context){
-    HubNetClientExtension.client match {
-      case None => Some(
-        try HubNetClientExtension.client = Some(BasicClient(
-          userId=args(0).getString, clientType="COMPUTER",
-          ip = args(1).getString, port = args(2).getIntValue))
-        catch {
-          case t => t.printStackTrace(); throw t
-        }
-      )
-      case _ => sys.error("already connected. use disconnect first.")
-    }
+    HubNetClientExtension.connect(args(0).getString, args(1).getString, args(2).getIntValue)
   }
 }
 
 class Disconnect extends DefaultCommand {
   override def getSyntax = commandSyntax(Array())
-  def perform(args: Array[Argument], context: Context){
-    HubNetClientExtension.client match {
-      case Some(client) =>
-        client.close("feel like it.")
-      case _ =>
-    }
-  }
+  def perform(args: Array[Argument], context: Context){ HubNetClientExtension.disconnect() }
 }
 
 class Send extends DefaultCommand {
   override def getSyntax = commandSyntax(Array(TYPE_STRING, TYPE_WILDCARD))
   def perform(args: Array[Argument], context: Context){
-    HubNetClientExtension.client match {
-      case Some(client) => client.sendActivityCommand("Button", args(0).getString, args(1).get)
-      case _ => sys.error("not connected")
-    }
+    HubNetClientExtension.send(args(0).getString, args(1).get)
+  }
+}
+
+class ClickButton extends DefaultCommand {
+  override def getSyntax = commandSyntax(Array(TYPE_STRING))
+  def perform(args: Array[Argument], context: Context){
+    HubNetClientExtension.send(args(0).getString, false.asInstanceOf[AnyRef])
   }
 }
